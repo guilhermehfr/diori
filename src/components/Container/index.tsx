f693553637206338337d6ded219bc60d640b5938
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-05 05:02:44 -0400

    feat: create Container component for layout structure

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,17 @@
+import clsx from "clsx";
+
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+export const Container = ({ children }: ContainerProps) => {
+  return (
+    <>
+      <div className={clsx("min-h-screen", "text-slate-900", "bg-slate-100")}>
+        <div className={clsx("max-w-screen-5xl text-center mx-auto")}>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+};
