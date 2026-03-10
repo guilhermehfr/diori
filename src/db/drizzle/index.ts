6e63bb040ae630899e6e3d26d0c0f98cad5c2295
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-10 13:49:35 -0400

    feat(db): set up SQLite database with Drizzle ORM and define posts schema
    - Removed ignored built dependencies from pnpm workspace configuration.
    - Added Drizzle configuration for SQLite database migrations and schema.
    - Created database connection and ORM setup in drizzle/index.ts.
    - Defined posts table schema in schemas.ts with appropriate fields and types.

---
+++ src/db/drizzle/index.ts
@@ -0,0 +1,16 @@
+import { resolve } from "path";
+import Database from "better-sqlite3";
+import { drizzle } from "drizzle-orm/better-sqlite3";
+import { postsTable } from "./schemas";
+
+const sqliteDatabasePath = resolve(process.cwd(), "db.sqlite3");
+const sqliteDatabase = new Database(sqliteDatabasePath);
+
+export const drizzleDb = drizzle(sqliteDatabase, {
+  schema: {
+    posts: postsTable,
+  },
+  logger: true,
+});
+
+export * from "./schemas";
