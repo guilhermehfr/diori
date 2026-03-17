0a651d968218a5f81e5c994ac0706a61a2f5f8f1
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-17 16:36:15 -0400

    feat: add admin utilities and new helpers
    Introduce admin PostList components, Delete button, and new logColor utility for improved admin and maintenance development support.

---
+++ src/components/admin/PostsListAdmin/index.tsx
@@ -0,0 +1,37 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+import { getAllPublicPostsAdmin } from "@/src/lib/post/queries/admin";
+
+import { DeletePostButton } from "@/src/components/admin/DeletePostButton";
+
+export async function PostsListAdmin() {
+  const posts = await getAllPublicPostsAdmin();
+
+  return (
+    <div className="mb-16">
+      {posts.map((post) => {
+        return (
+          <div
+            className={clsx(
+              "py-2 px-2",
+              !post.published && "bg-slate-300",
+              "flex gap-2 items-center justify-between",
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            {!post.published && (
+              <span className="text-xs text-slate-600 italic">
+                (Não publicado)
+              </span>
+            )}
+
+            <DeletePostButton id={post.id} title={post.title} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
