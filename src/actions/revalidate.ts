b62a0a8d0f765a53f0c1369480142deffa95c94b
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-18 19:49:20 -0400

    Remove deprecated revalidation file

--- src/actions/revalidate.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { revalidateTag } from "next/cache";
-
-export async function revalidatePosts() {
-  // Revalidate the cache for the specific post slug
-  revalidateTag(`posts`);
-  revalidateTag(`post-morning-routine-of-highly-effective-people`);
-}
