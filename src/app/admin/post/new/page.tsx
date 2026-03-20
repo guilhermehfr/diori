358c2f77b6f477db8fa2573137f5e15f236e458e
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-20 18:05:49 -0400

    feat: Admin UI enhancements (post form & navigation)

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,49 @@
+import { Button } from "@/src/components/Button";
+import { InputCheckbox } from "@/src/components/InputCheckbox";
+import { InputText } from "@/src/components/InputText";
+
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
-  return <div className="py-16 text-6xl">AdminPostNewPage</div>;
+  return (
+    <form action="" className="mb-16">
+      <div className="flex flex-col gap-6">
+        <InputText
+          labelText="Nome"
+          placeholder="Digite seu nome"
+          type="password"
+        />
+        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
+
+        <InputCheckbox labelText="Sobrenome" />
+
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+        />
+        <InputText
+          disabled
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          readOnly
+        />
+        <InputText
+          labelText="Sobrenome"
+          placeholder="Digite seu sobrenome"
+          defaultValue="Olá mundo"
+          readOnly
+        />
+
+        <div className="mt-4">
+          <Button type="submit">Enviar</Button>
+        </div>
+      </div>
+    </form>
+  );
 }
