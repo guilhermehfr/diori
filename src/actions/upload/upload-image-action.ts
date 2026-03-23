d643c0e59273f5bb309a0514f97fb4abf6da37e5
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-23 17:25:58 -0400

    feat: introduce image upload action and related constants

---
+++ src/actions/upload/upload-image-action.ts
@@ -0,0 +1,59 @@
+"use server";
+
+import {
+  IMAGE_SERVER_URL,
+  IMAGE_UPLOAD_DIRECTORY,
+  IMAGE_UPLOAD_MAX_SIZE,
+} from "@/src/lib/constants";
+import { mkdir, writeFile } from "fs/promises";
+import { extname, resolve } from "path";
+
+type UploadImageActionResult = {
+  url: string;
+  error: string;
+};
+
+export async function uploadImageAction(
+  formData: FormData,
+): Promise<UploadImageActionResult> {
+  const makeResult = ({ url = "", error = "" }) => ({ url, error });
+
+  if (!(formData instanceof FormData)) {
+    return makeResult({ error: "Invalid data" });
+  }
+
+  const file = formData.get("file");
+
+  if (!(file instanceof File)) {
+    return makeResult({ error: "Invalid file" });
+  }
+
+  if (file.size > IMAGE_UPLOAD_MAX_SIZE) {
+    return makeResult({ error: "File too large" });
+  }
+
+  if (!file.type.startsWith("image/")) {
+    return makeResult({ error: "Invalid image" });
+  }
+
+  const imageExtension = extname(file.name);
+  const uniqueImageName = `${Date.now()}${imageExtension}`;
+
+  const uploadFullPath = resolve(
+    process.cwd(),
+    "public",
+    IMAGE_UPLOAD_DIRECTORY,
+  );
+  await mkdir(uploadFullPath, { recursive: true });
+
+  const fileArrayBuffer = await file.arrayBuffer();
+  const buffer = Buffer.from(fileArrayBuffer);
+
+  const fileFullPath = resolve(uploadFullPath, uniqueImageName);
+
+  await writeFile(fileFullPath, buffer);
+
+  const url = `${IMAGE_SERVER_URL}/${IMAGE_UPLOAD_DIRECTORY}/${uniqueImageName}`;
+
+  return makeResult({ url });
+}
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,1 +1,3 @@
-export const MAX_UPLOAD_IMAGE_SIZE = 921600; // 900 KB * 1024
+export const IMAGE_UPLOAD_MAX_SIZE = 921600; // 900 KB * 1024
+export const IMAGE_SERVER_URL = "http://localhost:3000";
+export const IMAGE_UPLOAD_DIRECTORY = "uploads";
